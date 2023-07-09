a68239247dd8803308ad599d2ba1e9ee6d8d04d7
Carlos <cesosas@live.com.mx>
2023-07-08 20:23:48 -0600

    Initial commit: Primer avance del proyecto

---
+++ ConversionTools/src/main/Divisa.java
@@ -0,0 +1,15 @@
+package main;
+
+public enum Divisa {
+	MXN("MXN - Peso mexicano", "Pesos Mexicanos"), USD("USD - Dólar estadounidense", "Dólares estadounidenses"),
+	EUR("EUR - Euro", "Euros"), GBP("GBP - Libra esterlina", "Libras esterlinas"),
+	JPY("JPY - Yen Japonés", "Yenes japoneses"), KRW("KRW - Won surcoreano", "Wones surcoreanos");
+
+	public final String label;
+	public final String nombre;
+
+	private Divisa(String descripcion, String nombrePlural) {
+		this.label = descripcion;
+		this.nombre = nombrePlural;
+	}
+}
